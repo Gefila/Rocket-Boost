1669327edeba68f489af447cae104e78c6892538
gefila <gefilazonapranata@gmail.com>
2025-02-21 02:37:57 +0700

    41. Rotate The Rocket

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustSpeed = 100f;
+    [SerializeField] float rotationStrength = 100f;
     Rigidbody rb;
 
     private void Start()
@@ -15,14 +17,39 @@ public class Movement : MonoBehaviour
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustSpeed * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+        else if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationStrength)
+    {
+        transform.Rotate(Vector3.forward * rotationStrength * Time.fixedDeltaTime);
     }
 }
